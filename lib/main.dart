9a0f83bf069690bf0326281865ce0616b0b01fb1
Joao3run <57202776+Joao3run@users.noreply.github.com>
2021-05-09 16:11:52 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:marvel_heroes_flutter/app/app_module.dart';
 import 'package:marvel_heroes_flutter/app/app_widget.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     ModularApp(
       module: AppModule(),
